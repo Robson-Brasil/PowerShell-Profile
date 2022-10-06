88e6d6e6dacc252b1f45e188a8bb55bbad6347fd
Robson-Brasil <Robson.Brasil@outlook.com>
2022-10-05 20:22:37 -0400

    Profile Atualizado

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -42,42 +42,42 @@ if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
 #(Invoke-WebRequest "https://raw.githubusercontent.com/kiedtl/winfetch/master/winfetch.ps1" -UseBasicParsing).Content.Remove(0,1) | Invoke-Expression
 
 # Functions
-function procurar() {winget search $args}
-function update()   {winget upgrade $args}
-function upgrade()  {winget upgrade $args}
-function atualizar(){winget upgrade --all $args}
-function instalar() {winget install $args}
+function procurar()      {winget search $args}
+function update()        {winget upgrade $args}
+function upgrade()       {winget upgrade $args}
+function atualizar()     {winget upgrade --all $args}
+function desinstalar()   {winget uninstall $args}
 #Edição dos Profiles do WindowsTerminal e PowerShell
-function profile()  {code $PROFILE}
+function profile()       {code $PROFILE}
 function settings.json() {code "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"}
 #Arquivo de Historico do powerShell
-function history()  {code "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"}
+function history()       {code "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"}
 
-function ..()       {Set-Location ".."}
-function ....()     {Set-Location "..\.."}
-function ......()   {Set-Location "..\..\.."}
+function ..()            {Set-Location ".."}
+function ....()          {Set-Location "..\.."}
+function ......()        {Set-Location "..\..\.."}
 
-function ll()       {Get-ChildItem | Format-Table}
-function la()       {Get-ChildItem | Format-Wide}
-function lb()       {Get-ChildItem | Format-List}
+function ll()            {Get-ChildItem | Format-Table}
+function la()            {Get-ChildItem | Format-Wide}
+function lb()            {Get-ChildItem | Format-List}
 
 #GitHub Comandos
-function init()     {git init $args}
-function status()   {git status $args}
-function add()      {git add . $args}
-function commit()   {git commit -m  $args}
-function branch()   {git branch -M master  $args}
-function remote()   {git remote add origin  $args}
-function push()     {git push -u origin master  $args}
-function github()   {Set-Location "D:\GitRepositorio"}
+function init()          {git init $args}
+function status()        {git status $args}
+function add()           {git add . $args}
+function commit()        {git commit -m  $args}
+function branch()        {git branch -M master  $args}
+function remote()        {git remote add origin  $args}
+function push()          {git push -u origin master  $args}
+function github()        {Set-Location "D:\GitRepositorio"}
 
 # Compute file hashes - useful for checking successful downloads
-function md5        {Get-FileHash -Algorithm MD5 $args}
-function sha1       {Get-FileHash -Algorithm SHA1 $args}
-function sha256     {Get-FileHash -Algorithm SHA256 $args}
+function md5             {Get-FileHash -Algorithm MD5 $args}
+function sha1            {Get-FileHash -Algorithm SHA1 $args}
+function sha256          {Get-FileHash -Algorithm SHA256 $args}
 
-function tail       {Get-Content $args -Tail 30 -Wait}
-function take       {
+function tail            {Get-Content $args -Tail 30 -Wait}
+function take            {
   New-Item -ItemType directory $args
   Set-Location "$args"
 }
